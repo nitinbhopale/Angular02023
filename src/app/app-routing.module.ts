26aa61564ea62a16730dbb067ddd5d77ff33d561
nitinbhopale <nitinbhopale53@gmail.com>
2023-06-11 19:48:28 +0530

    Hotel Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login',pathMatch: 'full' //Login.component.html
+  },
+  {
+   path: 'login', component: LoginComponent //Login.component.html
+  },
+ {
+   path: 'signup', component: SignupComponent //signup.component.html 
+ }, 
+ {
+   path:'restaurent' , component: RestaurentDashComponent //rest-dash.component.html
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
